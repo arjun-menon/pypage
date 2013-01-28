0789ec629cc8e320361027caa19052f3ebcbfda1
Arjun G. Menon <contact@arjungmenon.com>
2013-01-28 09:37:08 +0530

    Fixed __all__ (pypage.py)

--- pypage.py
+++ pypage.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = ['pypage', 'pypage_multi']
+__all__ = ['pypage', 'pypage_files']
 
 import sys, imp, re
 
