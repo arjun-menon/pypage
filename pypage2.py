c073c8e61e5c8de76984d6b6ce843cfd745fe903
Arjun G. Menon <contact@arjungmenon.com>
2014-07-11 23:32:38 +0530

    pypage2: abstract syntax tree builder done.

--- pypage2.py
+++ pypage2.py
@@ -16,41 +16,106 @@
 
 import string, sys, os
 
-class TextNode(object):
+def indent(lines, level=1, spaces=4):
+    return '\n'.join( ' '  * spaces * level + line for line in lines.splitlines())
+
+def indent2(lines, level=1, spaces=4):
+    return '\n'.join( ' '  * spaces * level + line for line in lines.split('\n'))
+
+class RootNode(object):
     def __init__(self):
-        self.src = ""
+        self.children = list()
+    def __repr__(self):
+        return "Tree:\n" + indent('\n'.join(repr(child) for child in self.children))
+
+class TextNode(object):
+    """
+    A leaf node containing text.
+    """
+    def __init__(self):
+        self.src = str()
+    def __repr__(self):
+        return 'Text:\n' + indent(self.src) + '\n'
 
 class DelimitedNode(object):
+    """
+    A type representing all delimited nodes.
+
+    Derived classes must have:
+        open_delim: string containig the opening delimiter
+        close_delim: string containig the closing delimiter
+
+        src: the body of the delimited tag
+        loc: location (line & column numbers) of the 
+             opening delimiter in the source
+    """
     def __init__(self):
         pass
 
 class CodeNode(DelimitedNode):
+    """
+    A leaf node containing Python code.
+    """
     open_delim, close_delim = '{{', '}}'
+
     def __init__(self, loc):
         self.loc = loc
-        self.src = ""
+        self.src = str()
+    def __repr__(self):
+        return 'Code:\n' + indent(self.src) + '\n'
 
 class TagNode(DelimitedNode):
+    """
+    A node containing special directives.
+    """
     open_delim, close_delim = '{%', '%}'
+
     def __init__(self, loc):
         self.loc = loc
-        self.src = ""
+        self.src = str()
+        self.children = list()
+    def __repr__(self):
+        return "{%% %s %%}:\n" % self.src + indent('\n'.join(repr(child) for child in self.children))
+
+class CloseTagNode(object):
+    """
+    A TagNode with an empty body is replaced with this.
+    """
+    def __init__(self):
+        self.src = None
+    def __repr__(self):
+        return 'CloseTagNode.\n'
 
 delimitedNodeTypes = [CodeNode, TagNode]
 open_delims = { t.open_delim : t for t in delimitedNodeTypes }
 
-class IncompleteDelimitedNode(Exception):
+class PypageSyntaxError(Exception):
+    def __init__(self):
+        self.description = 'undefined'
+    def __str__(self):
+        return "Syntax Error: " + self.description
+
+class NodeIncompleteDelimited(PypageSyntaxError):
     def __init__(self, node):
         self.node = node
-    def __str__(self):
-        return "Syntax Error: Missing closing '%s' for opening '%s' at line %d, column %d." % ( 
+        self.description = "Missing closing '%s' for opening '%s' at line %d, column %d." % ( 
             self.node.close_delim, self.node.open_delim, 
             self.node.loc[0], self.node.loc[1] )
+
+class UnboundCloseTag(PypageSyntaxError):
+    def __init__(self, node):
+        self.node = node
+        self.description = "UnboundCloseTag"
+
+class UnclosedTag(PypageSyntaxError):
+    def __init__(self, node):
+        self.node = node
+        self.description = "UnclosedTag"
 
 def lex(src):
     tokens = list()
 
-    current_node = None
+    node = None
 
     i = 0
     line, line_c = 1, 0
@@ -68,56 +133,90 @@ def lex(src):
         # to find a node to belong to. We look for 
         # any valid opening delimiters, and if there 
         # are none, then we become a TextNode.
-        if not current_node:
+        if not node:
             if c2 in open_delims.keys():
-                current_node = open_delims[c2]((line, line_c))
-                current_node.src += c2
+                node = open_delims[c2]((line, line_c))
                 i += 2
                 continue
             else:
-                current_node =  TextNode()
+                node =  TextNode()
 
         # If we're in a TextNode currently, then we look 
         # for opening delimiters that'll end this TextNode.
-        if isinstance(current_node, TextNode):
+        if isinstance(node, TextNode):
             if c2 in open_delims.keys():
-                tokens.append(current_node)
-                current_node = open_delims[c2]((line, line_c))
-                current_node.src += c2
+                tokens.append(node)
+                node = open_delims[c2]((line, line_c))
                 i += 2
                 continue
 
         # If we're in a DelimitedNode, then we look for 
         # closing delimiters that'll complete this node.
-        if isinstance(current_node, DelimitedNode):
-            if c2 == current_node.close_delim:
-                current_node.src += c2
-                tokens.append(current_node)
-                current_node = None
+        if isinstance(node, DelimitedNode):
+            if c2 == node.close_delim:
+                # If we're in a TagNode, will strip node.src
+                if isinstance(node, TagNode):
+                    node.src = node.src.strip()
+
+                    if not node.src:
+                        node = CloseTagNode()
+
+                tokens.append(node)
+                node = None
                 i += 2
                 continue
 
-        current_node.src += c
-        i += 1
-
-    if current_node:
-        if isinstance(current_node, TextNode):
-            tokens.append(current_node)
-            current_node = None
+        if i < len(src) - 2:
+            node.src += c
+            i += 1
         else:
-            raise IncompleteDelimitedNode(current_node)
+            node.src += c2
+            i += 2
 
-    print '\n'.join( repr(node) + ": " + repr(node.src) for node in tokens)
+    if node:
+        if isinstance(node, TextNode):
+            tokens.append(node)
+            node = None
+        else:
+            raise NodeIncompleteDelimited(node)
 
+    return tokens
+
+def build_tree(node, toks):
+    try:
+        while True:
+            tok = toks.next()
+
+            if isinstance(tok, CloseTagNode):
+                if isinstance(node, TagNode):
+                    return
+                else:
+                    raise UnboundCloseTag(node)
+
+            node.children.append(tok)
+
+            if isinstance(tok, TagNode):
+                build_tree(tok, toks)
+    
+    except StopIteration:
+        if not isinstance(node, RootNode):
+            raise UnclosedTag(node)
 
 def execute(src):
     try:
-        lex(src)
-    except IncompleteDelimitedNode as e:
+        tokens = lex(src)
+    
+        tree = RootNode()
+
+        tokens_iterator = iter(tokens)
+
+        build_tree(tree, tokens_iterator)
+
+    except PypageSyntaxError as e:
         print e
         return
 
-    return None
+    print tree
 
 if __name__ == "__main__":
     import argparse
