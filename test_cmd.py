6d676d3def6cf879d8281be893cbec8e2322c8e7
Arjun G. Menon <contact@arjungmenon.com>
2015-09-14 16:03:51 -0400

    test_cmd.py: move copyright notice to the top

--- test_cmd.py
+++ test_cmd.py
@@ -2,6 +2,22 @@
 # -*- coding: utf-8 -*-
 
 #
+# Copyright (C) 2014 Arjun G. Menon
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
 #  Command-line Testing Tool
 #  =========================
 #  This is a simple tool for testing command-line tools.
@@ -30,22 +46,6 @@
 #  it in the directory containing your test cases. As a perk, 
 #  if an arguments maps to a non-string value, that value is 
 #  passed in as JSON.
-#
-
-#
-# Copyright (C) 2014 Arjun G. Menon
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 
 from __future__ import print_function
