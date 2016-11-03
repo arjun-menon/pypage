e076847bd6a727938db1637357491b6ecc35701d
Arjun G. Menon <contact@arjungmenon.com>
2016-11-03 17:07:27 -0400

    Update setup.py to auto-construct version & download_url

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@
 
 from setuptools import setup
 
+repo_url = 'https://github.com/arjun-menon/pypage'
+
+version_tag = open('docs/version.txt').read().strip()
+assert version_tag[0] == 'v'
+version = version_tag[1:]
+
+download_url = repo_url + '/archive/' + version_tag + '.tar.gz'
+
 setup(name='pypage',
-      version='2.0.0',
+      version=version,
       description=' Light-weight Python Templating Engine',
       # long_description=open('README.rst').read(),
-      url='https://github.com/arjun-menon/pypage',
+      url=repo_url,
+      download_url=download_url,
       author='Arjun G. Menon',
       author_email='contact@arjungmenon.com',
       keywords='templating enigne text processing static generator',
